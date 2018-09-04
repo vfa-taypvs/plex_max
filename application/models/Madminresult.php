0367fe6afdf45ca8e2749e27d21dca2dceaab030
Son Tay <vfa.taypvs@gmail.com>
2018-09-04 22:01:28 +0700

    Upload NEw Files

---
+++ application/models/Madminresult.php
@@ -0,0 +1,50 @@
+<?php
+class Madminresult extends CI_Model{
+    protected $_table = 'personals';
+    public function __construct(){
+        parent::__construct();
+    }
+
+    public function getListPersonals(){
+      $this->db->select('*');
+      return $this->db->get($this->_table)->result_array();
+    }
+
+    public function getListPersonalsWithLang($lang){
+      $this->db->where('lang',$lang);
+      $this->db->select('*');
+      return $this->db->get($this->_table)->result_array();
+    }
+
+    public function getPersonalsAtId($id){
+      $this->db->where('item_id',$id);
+      $this->db->select('*');
+      return $this->db->get($this->_table)->result_array();
+    }
+
+    public function getListColors(){
+      $this->db->where('lang','en');
+      $this->db->select('color');
+      return $this->db->get($this->_table)->result_array();
+    }
+
+    public function getListResultsNameColor(){
+      $this->db->where('lang','en');
+      $this->db->select('color, name');
+      return $this->db->get($this->_table)->result_array();
+    }
+
+    public function updatePersonalInfo($id,$data_update, $lang){
+      $this->db->where('item_id',$id);
+      $this->db->where('lang',$lang);
+      $this->db->update($this->_table,$data_update);
+    }
+
+    function removeTestAtId($id)
+    {
+       $this->db->where('item_id', $id);
+       $this->db->delete($this->_table);
+    }
+
+}
+?>
